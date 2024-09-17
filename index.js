4cd70d6b127d96165e04facfcc7c78d9cf5b4cb0
simperiale <ssimperiale@gmail.com>
2024-09-16 21:26:09 -0300

    index

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
-const Koa = require('koa');
-const Router = require('koa-router');
+const express = require('express');
+const app = express();
 
-const app = new Koa();
-const router = new Router();
-const sum = require('./sum');
-
-router.get('/add/:a/:b', (ctx, next) => {
-  const result = sum(parseFloat(ctx.params.a), parseFloat(ctx.params.b));
-  return ctx.body = { result };
+// Ruta básica para la página principal
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-app
-  .use(router.routes())
-  .use(router.allowedMethods());
-
-app.listen(process.env.PORT || 3000);
+// Puerto en el que escucha la app
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}`);
+});
